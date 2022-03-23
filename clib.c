49ae11d5035965e76a30cb3080c426e9e3ca4360
Francisco Boni Neto <boboniboni@gmail.com>
2022-03-23 05:28:35 -0300

    Small fixes & housekeeping.

--- clib.c
+++ clib.c
@@ -4,6 +4,10 @@ uint32_t c_always_inlined() {
     return 4;
 }
 
-__attribute__((noinline)) uint32_t c_never_inlined() {
+// __attribute__((noinline)) uint32_t c_never_inlined() {
+//     return 4;
+//}
+
+uint32_t c_never_inlined() {
     return 4;
 }
--- main.rs
+++ main.rs
@@ -1,3 +1,5 @@
+extern crate interesting;
+
 #[link(name = "xyz")]
 extern "C" {
     fn c_always_inlined() -> u32;
@@ -8,4 +10,5 @@ fn main() {
     unsafe {
         println!("blub: {}", c_always_inlined() + c_never_inlined());
     }
+    interesting::hello();
 }
